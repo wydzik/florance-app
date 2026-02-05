45d34f1f1d86790327eed59a43210741ef3eadf7
Wydzik <l.gryinternet@gmail.com>
2026-02-05 21:55:01 +0100

    Use DATABASE_URL for production

--- Florance_app/Florance_app/settings.py
+++ Florance_app/Florance_app/settings.py
@@ -93,15 +93,19 @@ WSGI_APPLICATION = 'Florance_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env('DATABASE_NAME'),
+#         'USER': env('DATABASE_USER'),
+#         'PASSWORD': env('DATABASE_PASSWORD'),
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
+    "default": env.db()
 }
 
 
