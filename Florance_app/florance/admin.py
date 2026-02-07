5dc3204e804c5a34b2b71a90d827cf561f709774
Wydzik <l.gryinternet@gmail.com>
2026-02-07 23:04:57 +0100

    add admin

--- Florance_app/florance/admin.py
+++ Florance_app/florance/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Florysta, Pracownia, Realizacja, Pracownicy, Kandydat, StatusKandydata, StatusPrzypisania, \
+                    RealizacjaPlik, Powiadomienie
 
-# Register your models here.
+admin.site.register(Florysta)
+
+
+@admin.register(Florysta)
+class FlorystaAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "avatar")
+
+
+admin.site.register(Pracownia)
+admin.site.register(Realizacja)
+admin.site.register(Pracownicy)
+admin.site.register(Kandydat)
+admin.site.register(StatusKandydata)
+admin.site.register(StatusPrzypisania)
+admin.site.register(RealizacjaPlik)
+admin.site.register(Powiadomienie)
